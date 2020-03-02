950757c4064aec7598965c02e5f1ba37dd9e4323
jhonatancruzmail <61705800+jhonatancruzmail@users.noreply.github.com>
2020-03-02 15:54:13 -0500

    comentario

--- holamundo/holamundo/Program.cs
+++ holamundo/holamundo/Program.cs
@@ -6,7 +6,7 @@ class Program
     {
         Console.WriteLine("hola mundo");
         Console.WriteLine("este es mi primer programa editado desde git hub");
-        Console.WriteLine("esta es una prueba de controld e versiones");
+        Console.WriteLine("esta es una prueba de controld de versiones");
         Console.ReadLine();
 
     }
