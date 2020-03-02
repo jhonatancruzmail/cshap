ccf15926196de4f5b2517bbd93a597ce68c2412f
jhonatan_stiven <jhonatancruzmail@gmail.com>
2020-03-02 14:53:36 -0500

    subida de carpeta atributos

---
+++ atributos/atributos/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+
+    static void Main(string[] args)
+
+    {
+        aprendiz apsena = new aprendiz();
+        apsena.Nombres = "jhonatan stiven";
+        apsena.apellidos = "stiven cruz";
+        apsena.Direccion = "cll 2 20-39 villas de snatamarta";
+        saludo(apsena); 
+    }
+    private static void saludo(aprendiz apsena)
+    {
+        Console.WriteLine("bienvenido, sr(a )" + apsena.Nombres);
+        Console.WriteLine(apsena.apellidos);
+         Console.WriteLine(apsena.Direccion);
+        Console.ReadLine();
+    }
+}
+
---
+++ atributos/atributos/aprendiz.cs
@@ -0,0 +1,29 @@
+﻿using System;
+class aprendiz
+{    public string TipoIdentificacion { get; set; }
+    public string NumeroIdentificacion { get; set; }
+    public string Nombres { get; set; }
+    public string apellidos { get; set; }
+    public DateTime FechaNacimiento { get; set; }
+    public string genero { get; set;} 
+    public string IDFicha { get; set; }
+    public string estado { get; set; }
+    public string EStadoCivil { get; set; }
+    public string Direccion { get; set; }
+    public string RH { get; set; }
+    public string Email { get; set; }
+    public string Telefono { get; set; }
+    public string TipoPoblacion { get; set; }
+    public string DepartamentoResidencia { get; set; }
+    public string DepartamentoNacimineto { get; set; }
+    public string municipionResidencia { get; set; }
+    public string municipioNacimiento { get; set; }
+    public string Estrato { get; set; }
+    public string EPS { get; set; }
+    public string TipoRegimen { get; set; }
+    public DateTime FechaIngreso { get; set; }
+    public string Discapacidad { get; set; }
+    public string LibretaMilitar { get; set; }
+}
+
+
